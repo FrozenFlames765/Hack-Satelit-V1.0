393793873cfa84a1157acc2adee183e6526e7b10
FrozenFlames765 <53082860+FrozenFlames765@users.noreply.github.com>
2019-07-19 21:52:46 +0700

    Add files via upload

---
+++ Satelit.sh
@@ -0,0 +1,181 @@
+#!/usr/bin/bash
+#tools sederhana
+#prank hack satelit
+#by ./Frozen Flames
+clear
+
+
+echo " "
+echo  $blue "<|======================================|>"  |lolcat
+echo  $red    "|         Hack Satleit Nasa           |"  |lolcat
+echo  $red    "|                                      |"  |lolcat
+echo  $red    "| Author        : Frozen Flames        |"  |lolcat
+echo  $red    "| Contact       : 085275097993         |"  |lolcat
+echo  $red    "| Team Code     : Tim White Hat IND    |"  |lolcat
+echo  $red    "|    All Member Hacker White Hat IND   |"  |lolcat
+echo  $blue "<|======================================|>"  |lolcat
+sleep 1
+
+echo " "
+echo $green "============== $red MENU $green ================="  |lolcat
+echo $blue "===================================="                |lolcat
+echo $blue "|| $green 1. Geser Satelit palapa       $blue ||"    |lolcat
+echo $blue "|| $green 2. Geser Satelit Telkom       $blue ||"    |lolcat
+echo $blue "|| $green 3. Geser Satelit Asiasat      $blue ||"    |lolcat
+echo $blue "|| $green 4. Geser Satelit Nasa         $blue ||"    |lolcat
+echo $blue "|| $green 0. Exit The Program           $blue ||"    |lolcat
+echo $blue "===================================="                |lolcat
+echo $blue"╭──[Masukkan Pilihan Anda]>"    |lolcat
+read -p"╰───────./Masukan Wae Lh cuk=•>" gans;
+
+
+
+
+
+if [ $gans = 1 ] || [ $gans = 01 ]
+then
+echo $blue"╭──[Mau Kearah Mana cuk]>"  |lolcat
+read -p"╰───────./Frozen Flames=•>" arah;
+sleep 1
+echo $green"╭──[Mau Digeser Berapa Derajat woi]>"    |lolcat
+read -p"╰───────./Frozen Flames=•>" derajat;
+sleep 1
+echo $red "Loading"      |lolcat
+sleep 1
+echo $blue "10%"         |lolcat
+sleep 1
+echo $green "20%"         |lolcat
+sleep 1
+echo $cyan "30%"          |lolcat
+sleep 1
+echo $purple "40%"        |lolcat
+sleep 1
+echo $blue "50%"           |lolcat
+sleep 1
+echo $red "60%"             |lolcat
+sleep 1
+echo $green "70%"           |lolcat
+sleep 1
+echo $cyan "80%"            |lolcat
+sleep 1
+echo $purple "90%"         |lolcat
+sleep 1
+echo $blue "100%"          |lolcat
+sleep 1
+echo $green "Prosessing Done"      |lolcat
+sleep 1
+echo  $blue "Satelit Berhasil Digeser ke arah $arah sejauh $derajat derajat"        |lolcat 
+fi
+
+if [ $gans = 2 ] || [ $gans = 02 ]
+then
+echo $blue"╭──[Mau Kearah Mana boy]>"    |lolcat
+read -p"╰───────./Frozen Flames=•>" arah;
+sleep 1
+echo $green"╭──[Mau Digeser Berapa Derajat bujank]>"     |lolcat
+read -p"╰───────./Frozen Flames=•>" derajat;
+sleep 1
+echo $red "Loading"    |lolcat
+sleep 1
+echo $blue "10%"      |lolcat
+sleep 1
+echo $green "20%"     |lolcat
+sleep 1
+echo $cyan "30%"     |lolcat
+sleep 1
+echo $purple "40%"    |lolcat
+sleep 1
+echo $blue "50%"     |lolcat
+sleep 1
+echo $red "60%"      |lolcat
+sleep 1
+echo $green "70%"      |lolcat
+sleep 1
+echo $cyan "80%"       |lolcat
+sleep 1
+echo $purple "90%"    |lolcat
+sleep 1
+echo $blue "100%"      |lolcat
+sleep 1
+echo $green "Prosessing Done"      |lolcat
+sleep 1
+echo  $blue "Satelit Berhasil Digeser ke arah $arah sejauh $derajat derajat"      |lolcat
+fi
+
+if [ $gans = 3 ] || [ $gans = 03 ]
+then
+echo $blue"╭──[Mau Kearah Mana cuk]>"   |lolcat
+read -p"╰───────./Frozen Flames=•>" arah;
+sleep 1
+echo $green"╭──[Mau Digeser Berapa Derajat njer]>"   |lolcat
+read -p"╰───────./Frozen Flames=•>" derajat;
+sleep 1
+echo $red "Loading"     |lolcat
+sleep 1
+echo $blue "10%"    |lolcat
+sleep 1
+echo $green "20%"    |lolcat
+sleep 1
+echo $cyan "30%"     |lolcat
+sleep 1
+echo $purple "40%"   |lolcat
+sleep 1
+echo $blue "50%"     |lolcat
+sleep 1
+echo $red "60%"      |lolcat
+sleep 1
+echo $green "70%"      |lolcat
+sleep 1
+echo $cyan "80%"      |lolcat
+sleep 1
+echo $purple "90%"    |lolcat
+sleep 1
+echo $blue "100%"    |lolcat
+sleep 1
+echo $green "Prosessing Done"    |lolcat
+sleep 1
+echo  $blue "Satelit Berhasil Digeser ke arah $arah sejauh $derajat derajat"     |lolcat
+fi
+
+if [ $gans = 4 ] || [ $gans = 04 ]
+then
+echo $blue"╭──[Mau Kearah Mana]>"   |lolcat
+read -p"╰───────./Frozen Flames=•>" arah;
+sleep 1
+echo $green"╭──[Mau Digeser Berapa Derajat]>"    |lolcat
+read -p"╰───────./Frozen Flames=•>" derajat;
+sleep 1
+echo $red "Loading"   |lolcat
+sleep 1
+echo $blue "10%"     |lolcat
+sleep 1
+echo $green "20%"    |lolcat
+sleep 1
+echo $cyan "30%"    |lolcat
+sleep 1
+echo $purple "40%"   |lolcat
+sleep 1
+echo $blue "50%"    |lolcat
+sleep 1
+echo $red "60%"    |lolcat
+sleep 1
+echo $green "70%"   |lolcat
+sleep 1
+echo $cyan "80%"    |lolcat
+sleep 1
+echo $purple "90%"   |lolcat
+sleep 1
+echo $blue "100%"    |lolcat
+sleep 1
+echo $green "Prosessing Done"    |lolcat
+sleep 1
+echo  $blue "Satelit Berhasil Digeser ke arah $arah sejauh $derajat derajat"     |lolcat
+fi
+
+if [ $gans = 0 ] || [ $gans = 00 ]
+then
+echo  $red "Bay Bay Hacker.. "                                          |lolcat
+echo  $blue "Mampus Ip Lu Bakal Di Lacak Oleh Satelit"                  |lolcat
+echo  $green "Tools By Frozen Flames"                                   |lolcat
+exit 
+fi
